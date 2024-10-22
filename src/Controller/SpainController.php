0e7e4a96b9f92ca2872149397fbdb18c3d1102cf
Mehdi <mehdilaz109@gmail.com>
2024-10-22 17:55:49 +0200

    ADD Pagination

--- src/Controller/SpainController.php
+++ src/Controller/SpainController.php
@@ -4,17 +4,30 @@ namespace App\Controller;
 
 use App\Repository\SpainRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class SpainController extends AbstractController
 {
     #[Route('/spain', name: 'app_spain')]
-    public function index(SpainRepository $spainRepository): Response
+    public function index(SpainRepository $spainRepository, Request $request): Response
     {
-        $spainData = $spainRepository -> findAll();
+        // Get current page from query parameters
+        $page = $request->query->getInt('page', 1);
+        // Number of items per page
+        $limit = 5;
+        // Fetch the total number of items
+        $totalItems = $spainRepository->count([]);
+        // Fetch paginated results
+        $spainData = $spainRepository->findBy([], null, $limit, ($page - 1) * $limit);
+        // Calculate total pages
+        $totalPages = ceil($totalItems / $limit);
+
         return $this->render('spain/index.html.twig', [
             'spainData' => $spainData,
+            'currentPage' => $page,
+            'totalPages' => $totalPages,
         ]);
     }
 }
